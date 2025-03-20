18659ed625f99b2865afe880d5a18cdf2a067661
Emmanuel802 <a.emmanuelll.17@gmail.com>
2025-03-20 13:56:24 +0200

    Final touches and Js comments

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+// Toggle Button And Icon Elements
 const toggleButton = document.getElementById("darkModeToggle");
 const icon = document.getElementById("icon");
 
+// Event Listener To Toggle the Button
 toggleButton.addEventListener("click", function () {
   const body = document.body;
   const container = document.querySelector(".container");
 
+  // Applies Dark Mode On Body And Container
   body.classList.toggle("dark-mode");
   container.classList.toggle("dark-mode");
 
+  // Icon Change For The Button
   if (body.classList.contains("dark-mode")) {
     icon.classList.replace("fa-moon", "fa-lightbulb");
   } else {
